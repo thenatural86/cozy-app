71ebb9aaa6b81ff3bf83e0d11aeb3788443c8f60
thenatural86 <philip.smalls86@gmail.com>
2021-01-07 10:20:48 -0500

    change title

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return <h4>cozy's corner</h4>
 }
 
 export default App
