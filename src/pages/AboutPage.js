6b27f5b1677d2bd463e671b6c67295b1d132dbca
thenatural86 <philip.smalls86@gmail.com>
2021-01-08 13:00:44 -0500

    render hero component

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,26 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='hero' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+            labore repudiandae dicta dolores eos magni iure quia, alias quae
+            nobis eius distinctio expedita iste numquam cum doloribus obcaecati
+            iusto. Molestias officia nisi quam facere cupiditate.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
 }
 
 const Wrapper = styled.section`
