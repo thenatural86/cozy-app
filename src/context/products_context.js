1087b2a296ccfabb17ca0f906958e8622463a920
thenatural86 <philip.smalls86@gmail.com>
2021-01-09 11:32:38 -0500

    fetch products data

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import React, { useContext, useEffect, useReducer } from 'react'
 import reducer from '../reducers/products_reducer'
 import { products_url as url } from '../utils/constants'
+
 import {
   SIDEBAR_OPEN,
   SIDEBAR_CLOSE,
@@ -28,6 +29,15 @@ export const ProductsProvider = ({ children }) => {
   const closeSidebar = () => {
     dispatch({ type: SIDEBAR_CLOSE })
   }
+
+  const fetchProducts = async (url) => {
+    const response = await axios.get(url)
+    console.log(response)
+  }
+
+  useEffect(() => {
+    fetchProducts(url)
+  }, [])
 
   return (
     <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
