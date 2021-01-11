e9ba09cb85541902537455da288fe8bd0405ae56
thenatural86 <philip.smalls86@gmail.com>
2021-01-11 13:07:44 -0500

    programmtic approach to render stars count

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -6,55 +6,32 @@ const Stars = ({ stars, reviews }) => {
   // console.log(reviews)
 
   // double ternary operators and manual approach
+  // ;<span>
+  //   {stars >= 1 ? <BsStarFill /> : stars >= 0.5 ? <BsStarHalf /> : <BsStar />}
+  // </span>
+
+  // programmatic approach
+  // Array.from() creates an array which has a length equal to the length property we that is passed in. 2nd arg is callback function that we call against every item. first param of callback is undefined represented as an '_', 2nd param is index
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    // index 0 - 4, every iteration index changes from 0 up to 4, hence index + 1.
+    // if stars is whole display whole star, if stars >= 1.5,2.5 ect, display half star, otherwise display empty star
+    const number = index + 0.5
+    return (
+      <span key={index}>
+        {stars >= index + 1 ? (
+          <BsStarFill />
+        ) : stars >= number ? (
+          <BsStarHalf />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    )
+  })
+  console.log(tempStars)
   return (
     <Wrapper>
-      <div className='stars'>
-        <span>
-          {stars >= 1 ? (
-            <BsStarFill />
-          ) : stars >= 0.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        <span>
-          {stars >= 2 ? (
-            <BsStarFill />
-          ) : stars >= 1.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        <span>
-          {stars >= 3 ? (
-            <BsStarFill />
-          ) : stars >= 2.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        <span>
-          {stars >= 4 ? (
-            <BsStarFill />
-          ) : stars >= 3.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        <span>
-          {stars >= 5 ? (
-            <BsStarFill />
-          ) : stars >= 4.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-      </div>
+      <div className='stars'>{tempStars}</div>
       <p className='reviews'>({reviews} customer reviews)</p>
     </Wrapper>
   )
