babc21a51b97108fb34e82e8e5a60f90d4ac0cbe
thenatural86 <philip.smalls86@gmail.com>
2021-01-12 11:44:55 -0500

    list and grid view functionality

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -32,8 +32,15 @@ export const FilterProvider = ({ children }) => {
   useEffect(() => {
     dispatch({ type: LOAD_PRODUCTS, payload: products })
   }, [products])
+
+  const setGridView = () => {
+    dispatch({ type: SET_GRIDVIEW })
+  }
+  const setListView = () => {
+    dispatch({ type: SET_LISTVIEW })
+  }
   return (
-    <FilterContext.Provider value={{ ...state }}>
+    <FilterContext.Provider value={{ ...state, setGridView, setListView }}>
       {children}
     </FilterContext.Provider>
   )
