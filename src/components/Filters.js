bf08db939b86ad8e8609fe25fff31b2a3eb16f31
thenatural86 <philip.smalls86@gmail.com>
2021-01-14 12:11:33 -0500

    company select tag

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -43,6 +43,7 @@ const Filters = () => {
             />
           </div>
           {/* end search input */}
+
           {/* categories */}
           <div className='form-control'>
             <h5>category</h5>
@@ -65,7 +66,20 @@ const Filters = () => {
             </div>
           </div>
           {/* end of categories */}
+
           {/* companies */}
+          <div className='form-control'>
+            <h5>company</h5>
+            <select name='company' value={company} onChange={updateFilters}>
+              {companies.map((c, index) => {
+                return (
+                  <option key={index} value={c}>
+                    {c}
+                  </option>
+                )
+              })}
+            </select>
+          </div>
           {/* end of companies */}
           {/* colors */}
           {/* end of colors */}
