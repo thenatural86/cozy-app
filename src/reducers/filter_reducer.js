c18e2ac7879d20f8d575f315cae3369035b62fdf
thenatural86 <philip.smalls86@gmail.com>
2021-01-14 14:59:58 -0500

    shipping and clear filters functionality

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -54,6 +54,19 @@ const filter_reducer = (state, action) => {
       return { ...state, filters: { ...state.filters, [name]: value } }
     case FILTER_PRODUCTS:
       return { ...state }
+    case CLEAR_FILTERS:
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          text: '',
+          company: 'all',
+          category: 'all',
+          color: 'all',
+          price: state.filters.max_price,
+          shipping: false,
+        },
+      }
     default:
       throw new Error(`No Matching "${action.type}" - action type`)
   }
