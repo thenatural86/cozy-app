3b734d6372e2ddb7b8f7700cbd1987048fde99b2
thenatural86 <philip.smalls86@gmail.com>
2021-01-15 15:28:19 -0500

    add to cart functionality in reducer

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -7,8 +7,40 @@ import {
 } from '../actions'
 
 const cart_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
+  switch (action.type) {
+    case ADD_TO_CART:
+      // passed in from add to cart
+      const { id, color, amount, product } = action.payload
+      const tempItem = state.cart.find((i) => i.id === id + color)
+      if (tempItem) {
+        const tempCart = state.cart.map((cartItem) => {
+          if (cartItem.id === id + color) {
+            let newAmount = cartItem.amount + amount
+            if (newAmount > cartItem.max) {
+              newAmount = cartItem.max
+            }
+            return { ...cartItem, amount: newAmount }
+          } else {
+            return cartItem
+          }
+        })
+        return { ...state, cart: tempCart }
+      } else {
+        // add new item to cart
+        const newItem = {
+          id: id + color,
+          name: product.name,
+          color,
+          amount,
+          image: product.images[0].url,
+          price: product.price,
+          max: product.stock,
+        }
+        return { ...state, cart: [...state.cart, newItem] }
+      }
+    default:
+      throw new Error(`No Matching "${action.type}" - action type`)
+  }
 }
 
 export default cart_reducer
