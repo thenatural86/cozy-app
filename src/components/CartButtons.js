ce20e1520dfa577ba87775064350c9ce98196acb
thenatural86 <philip.smalls86@gmail.com>
2021-01-18 12:43:01 -0500

    login/logout buttons

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -9,7 +9,7 @@ import { useUserContext } from '../context/user_context'
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext()
   const { total_items } = useCartContext()
-  const { logout, user, loginWithRedirect } = useUserContext()
+  const { logout, myUser, loginWithRedirect } = useUserContext()
 
   return (
     <Wrapper className='cart-btn-wrapper'>
@@ -20,16 +20,19 @@ const CartButtons = () => {
           <span className='cart-value'>{total_items}</span>
         </span>
       </Link>
-      <button type='button' className='auth-btn' onClick={loginWithRedirect}>
-        Login <FaUserPlus />
-      </button>
-      <button
-        type='button'
-        className='auth-btn'
-        onClick={() => logout({ returnTo: window.location.origin })}
-      >
-        Logout <FaUserMinus />
-      </button>
+      {myUser ? (
+        <button
+          type='button'
+          className='auth-btn'
+          onClick={() => logout({ returnTo: window.location.origin })}
+        >
+          Logout <FaUserMinus />
+        </button>
+      ) : (
+        <button type='button' className='auth-btn' onClick={loginWithRedirect}>
+          Login <FaUserPlus />
+        </button>
+      )}
     </Wrapper>
   )
 }
