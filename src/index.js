384fb435c2fc8f97c5b28fa805ee67d54a73c3ce
thenatural86 <philip.smalls86@gmail.com>
2021-01-18 12:00:17 -0500

    auth0 setup

--- src/index.js
+++ src/index.js
@@ -8,13 +8,22 @@ import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
 
+// dev-xficzv3o.us.auth0.com
+// Cs1qh2TuN4O6Rsb0DabfkgLYT98x3Nmk
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain='dev-xficzv3o.us.auth0.com'
+    clientId='Cs1qh2TuN4O6Rsb0DabfkgLYT98x3Nmk'
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>,
   document.getElementById('root')
 )
