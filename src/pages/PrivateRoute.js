e3d98369e3703d002d7d8ccf7f2f210391c1e88c
thenatural86 <philip.smalls86@gmail.com>
2021-01-18 13:24:22 -0500

    checkout private route

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -2,7 +2,19 @@ import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import { useUserContext } from '../context/user_context'
 
-const PrivateRoute = () => {
-  return <h4>Private Route</h4>
+// rest operator
+const PrivateRoute = ({ children, ...rest }) => {
+  // console.log(children)
+  // console.log(rest)
+  const { myUser } = useUserContext()
+  // spread operator
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return myUser ? children : <Redirect to='/'></Redirect>
+      }}
+    ></Route>
+  )
 }
 export default PrivateRoute
