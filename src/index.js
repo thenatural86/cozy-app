73d4495fff74b30ec00d60f8f67b11d15f2b7e11
thenatural86 <philip.smalls86@gmail.com>
2024-06-03 18:27:06 -0300

    updates dependecies again

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { ProductsProvider } from './context/products_context'
@@ -17,7 +17,10 @@ import { Auth0Provider } from '@auth0/auth0-react'
 // old
 // dev-xficzv3o.us.auth0.com
 // Cs1qh2TuN4O6Rsb0DabfkgLYT98x3Nmk
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH_DOMAIN}
     clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
@@ -33,6 +36,5 @@ ReactDOM.render(
         </FilterProvider>
       </ProductsProvider>
     </UserProvider>
-  </Auth0Provider>,
-  document.getElementById('root')
+  </Auth0Provider>
 )
