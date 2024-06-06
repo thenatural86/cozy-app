dd216a674c7410aed75c3a83a7db4d2dda9b8cba
thenatural86 <philip.smalls86@gmail.com>
2024-06-06 10:15:38 -0300

    revert changes

--- src/index.js
+++ src/index.js
@@ -23,9 +23,7 @@ root.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH_DOMAIN}
     clientId={process.env.REACT_APP_CLIENT_ID}
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
+    redirectUri={window.location.origin}
     cacheLocation='localstorage'
   >
     <UserProvider>
