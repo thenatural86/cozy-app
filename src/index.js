8bb6aa285248749a7e0ef1af99eb6b178cd9193c
thenatural86 <philip.smalls86@gmail.com>
2024-06-07 12:30:46 -0300

    adds link to site

--- src/index.js
+++ src/index.js
@@ -8,19 +8,6 @@ import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
 
-// treyknoxx@gmail.com
-// +Cux%7$!VL%N++?
-
-// Domain
-// dev-itfjckjv62h81dxu.us.auth0.com
-
-// Client Id
-// BJWflfLTJZ7eO93Jwkt8g8L67SJBbxvF
-
-// old
-// dev-xficzv3o.us.auth0.com
-// Cs1qh2TuN4O6Rsb0DabfkgLYT98x3Nmk
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
